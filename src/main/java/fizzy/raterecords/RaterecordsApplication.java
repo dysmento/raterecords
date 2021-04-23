5b7de1acdfff049795246b7e6c4279318cfd47e7
Zach Thomas <zach.thomas@genesys.com>
2021-04-23 08:59:05 -0400

    add the basic project files for Spring Boot

---
+++ src/main/java/fizzy/raterecords/RaterecordsApplication.java
@@ -0,0 +1,13 @@
+package fizzy.raterecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaterecordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaterecordsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fizzy/raterecords/RaterecordsApplicationTests.java
@@ -0,0 +1,14 @@
+package fizzy.raterecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaterecordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+/**/
