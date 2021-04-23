25130ad1734d0dc039a847fbc03ec808375607c6
Zach Thomas <zach.thomas@genesys.com>
2021-04-23 10:09:44 -0400

    add test for parsing contents of a file

--- src/test/java/fizzy/raterecords/parsers/DelimiterParserTest.java
+++ src/test/java/fizzy/raterecords/parsers/DelimiterParserTest.java
@@ -3,8 +3,12 @@ package fizzy.raterecords.parsers;
 import fizzy.raterecords.model.Gender;
 import fizzy.raterecords.model.RateRecord;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,17 +25,29 @@ public class DelimiterParserTest {
     @Test
     void parsePipes() {
         String data = "Lincoln | Abraham | male | brown | 1809-02-12";
-        final DelimiterParser commaParser = new DelimiterParser("\\|");
-        final RateRecord lincoln = commaParser.parse(data);
+        final DelimiterParser parser = new DelimiterParser("\\|");
+        final RateRecord lincoln = parser.parse(data);
         assertEquals(canonicalLincoln(), lincoln);
     }
 
     @Test
     void parseSpaces() {
         String data = "Lincoln Abraham male brown 1809-02-12";
-        final DelimiterParser commaParser = new DelimiterParser(" ");
-        final RateRecord lincoln = commaParser.parse(data);
+        final DelimiterParser parser = new DelimiterParser(" ");
+        final RateRecord lincoln = parser.parse(data);
         assertEquals(canonicalLincoln(), lincoln);
+    }
+
+    @Test
+    void parseFile() throws Exception {
+        final DelimiterParser parser = new DelimiterParser("\\|");
+        final File pipeData = new ClassPathResource("pipeDelimited.txt").getFile();
+        final ArrayList<RateRecord> rateRecords = new ArrayList<>();
+        final Scanner scanner = new Scanner(pipeData);
+        while (scanner.hasNextLine()) {
+            rateRecords.add(parser.parse(scanner.nextLine()));
+        }
+        assertEquals(2, rateRecords.size());
     }
 
     private RateRecord canonicalLincoln() {
